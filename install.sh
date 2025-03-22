31ce28bb36b01669072f3fe4c93531475f65d628
grideperlevy <mr.ivandmi@yandex.ru>
2025-03-22 18:41:52 +0300

    prepare install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+author=igroman787
+repo=mytonprovider
+current_dir=$(pws)
+
 # colors
 COLOR='\033[92m'
 ENDC='\033[0m'
@@ -64,128 +68,90 @@ restart_yourself_via_root
 user=${input_user}
 echo "Using user: ${user}"
 
-check_go_version() { # перенести в локальный install_ton-storage.sh
-	go_mod_path=${1}
-	go_path=${2}
-	go_mod_text=$(cat ${go_mod_path}) || exit 1
-	need_version_text=$(echo "${go_mod_text}" | grep "go " | head -n 1 | awk '{print $2}')
-	current_version_text=$(${go_path} version | awk '{print $3}' | sed 's\go\\g')
-	echo "start check_go_version function, need_version: ${need_version_text}, current_version: ${current_version_text}"
-	current_version_1=$(echo ${current_version_text} | cut -d "." -f 1)
-	current_version_2=$(echo ${current_version_text} | cut -d "." -f 2)
-	current_version_3=$(echo ${current_version_text} | cut -d "." -f 3)
-	need_version_1=$(echo ${need_version_text} | cut -d "." -f 1)
-	need_version_2=$(echo ${need_version_text} | cut -d "." -f 2)
-	need_version_3=$(echo ${need_version_text} | cut -d "." -f 3)
-	if (( need_version_1 > current_version_1 )) || ((need_version_2 > current_version_2 )) || ((need_version_3 > current_version_3 )); then
-		install_go
-	fi
-}
-
-install_go() {
-  echo -e "${COLOR}[5/9]${ENDC} Installing Go"
-	arc=$(dpkg --print-architecture)
-	go_version_url=https://go.dev/VERSION?m=text
-	go_version=$(curl -s ${go_version_url} | head -n 1)
-	go_url=https://go.dev/dl/${go_version}.linux-${arc}.tar.gz
-	rm -rf /usr/local/go
-	wget -c ${go_url} -O - | tar -C /usr/local -xz
-}
-
-install_curl() {
-  echo -e "${COLOR}[2/9]${ENDC} Installing Curl"
+install_option_utils() {
   apt install curl -y
-}
-
-install_wget() {
-  echo -e "${COLOR}[3/9]${ENDC} Installing Wget"
   apt install wget -y
-}
-
-install_git() {
-  echo -e "${COLOR}[4/9]${ENDC} Installing Git"
   apt install git -y
 }
 
 check_python_version () {
-  echo "check python version"
-  p3_version=$(python3 --version | cut -d " " -f 2)
-  p3_major=$(echo ${p3_version} | cut -d "." -f 1)
-  if [ "${p3_major}" != "3" ]; then
+  major=$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1)
+  minor=$(python3 --version | cut -d " " -f 2 | cut -d "." -f 2)
+  if [ "${major}" != "3" ]; then
     install_python311
   fi
-  p3_minor=$(echo ${p3_version} | cut -d "." -f 2)
-  if [ ${p3_minor} -lt 11 ]; then
+  if [ ${minor} -lt 11 ]; then
     install_python311
   fi
 }
 
 install_python311() {
-  echo -e "${COLOR}[6/9]${ENDC} Installing Python 3.11"
-  if [ "$(uname)" > "Ubuntu 22.04" ]; then
-    apt update
-    apt install python3.11
-    apt install python3-pip
+  system_name=$(echo uname | cut -d " " -f 1)
+  if [ "${system_name}" = "Ubuntu" ]; then
+    version=$(echo uname | cut -d " " -f 2 | cut -d "." -f 1)
+    if [ "${version}" -ge 22 ] ; then
+      apt update
+      apt install python3.11
+      apt install python3-pip
+    else
+      apt install software-properties-common -y
+      add-apt-repository ppa:deadsnakes/ppa -y
+      apt update
+      apt install python3.11
+      apt install python3-pip
+    fi
+  elif [ "${system_name}" = "Debian" ]; then
+      apt install software-properties-common -y
+      add-apt-repository ppa:deadsnakes/ppa -y
+      apt update
+      apt install python3.11
+      apt install python3-pip
   fi
-  if (("$(uname)" = "Ubuntu 20.04")) || (( "$(uname)" = "Debian 11" )); then
-    apt install software-properties-common -y
-    add-apt-repository ppa:deadsnakes/ppa -y
-    apt update
-    apt install python3.11
-    apt install python3-pip
-  fi
-#  if (("$(uname)" = "CentOS")) || (( "$(uname)" = "Rocky" )); then
-#    dnf install -y gcc gcc-c++ make
-#    dnf install -y python3.11
-#  fi
-#  if (("$(uname)" = "Arch")); then
-#    pacman -S python
-#  fi
-
 }
 
+activate_venv() {
+  python3.11 -m venv venv
+  source "${1}/venv/bin/activate"
+}
+
+deactivate_venv() {
+  deactivate
+}
 
 install_requirements() {
-  echo -e "${COLOR}[8/9]${ENDC} Installing requirements"
-  pip311=$(echo pip3.${p3_minor})
-#  pip install --upgrade pip
-  $pip311 install -r mytonprovider/src/requirements.txt
+  pip install --upgrade pip
+  pip install -r "${1}/mytonprovider/src/requirements.txt"
 }
 
 download_mytonprovider() {
-  echo -e "${COLOR}[7/9]${ENDC} Downloading mytonprovider"
-  git clone https://github.com/igroman787/mytonprovider
+  cd "${1}"
+  git clone "https://github.com/${author}/${repo}"
 }
 
 launch_mtp() {
-  echo -e "${COLOR}[9/9]${ENDC} Installing mytonprovider"
-  # проверить что собран бинарник tonutils-storage
-  export STORAGE_CMD=/home/${user}/tonutils-storage  # изменить на абсолютный путь # передавать путь через аргументы
-
-  DEFAULT_STORAGE_PATH=$(pwd)
-  export DEFAULT_STORAGE_PATH # defaults vars in one file
-
-  export STORAGE_PROVIDER_CMD=./tonutils-storage
-  export TUNNEL_PROVIDER_CMD=./tonutils-storage
-
-  python3.11 -m mytonprovider/install.py
+  python "${1}/mytonprovider/install.py"
 }
 
 install_mtp() {
-  echo -e "${COLOR}[1/9]${ENDC} Preparing mytonprovider"
-  cd ${user}
+  cd "${user}"
 
+  echo -e "${COLOR}[1/6]${ENDC} Installing utils"
   install_option_utils
-#  install_wget
-#  install_git
 
-#  check_go_version
+  echo -e "${COLOR}[2/6]${ENDC} Check python version"
   check_python_version
 
-  download_mytonprovider
-  install_requirements
+  echo -e "${COLOR}[3/6]${ENDC} Activating virtual environment"
+  activate_venv "${current_dir}"
 
-  lauch_mtp
+  echo -e "${COLOR}[4/6]${ENDC} Downloading MyTonProvider"
+  download_mytonprovider "${current_dir}"
+
+  echo -e "${COLOR}[5/6]${ENDC} Installing requirements"
+  install_requirements "${current_dir}"
+
+  echo -e "${COLOR}[6/6]${ENDC} Launching MyTonProvider"
+  lauch_mtp "${current_dir}"
 }
 
 install_mtp
