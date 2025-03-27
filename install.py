0f6e5893f3be4be6ea3215d185fa12aed83da471
grideperlevy <mr.ivandmi@yandex.ru>
2025-03-27 21:40:04 +0300

    refactor

--- install.py
+++ install.py
@@ -1,41 +1,67 @@
-from src.questions import utils_selection, storage_cost, storage_path, storage_disk_space, traffic_cost
+from src.utils import get_disk_free_space
 from src import ton_storage, ton_storage_provider, ton_tunnel_provider
 
+from inquirer import Text, List, Path
 from typing import Any
 import inquirer
 from mypylib import Dict
 
 
 def ask() -> dict[str, Any]:
-    answers = []
-    util = inquirer.prompt([utils_selection])
-    answers.append(util)
-    match util["utils_selection"]:
+    util = inquirer.prompt([
+        List(
+            name="util",
+            message="Выберете утилиту",
+            choices=["TonStorage", "TonStorageProvider", "TonTunnelProvider"]
+        )
+    ])
+    answers = [util]
+    match util["util"]:
 
-        case "ton_storage":
-            answers.append(inquirer.prompt([storage_path]))
+        case "TonStorage":
+            answers.append(inquirer.prompt([
+                Path(
+                    name="storage_path",
+                    message=f"Ввод места хранения файлов ton_storage (по умолчанию: /var/tonstorage/)",
+                    default="/var/tonstorage",
+                )
+            ]))
 
-        case "ton_storage_provider":
-            answers.append(inquirer.prompt([storage_cost]))
-            answers.append(inquirer.prompt([storage_path]))
-            answers.append(inquirer.prompt([storage_disk_space]))
+        case "TonStorageProvider":
+            answers.append(inquirer.prompt([
+                Text(
+                    name="storage_cost",
+                    message="Сколько будет стоить хранения 1 Гб/мес ?"
+                ),
+                Path(
+                    name="storage_path",
+                    message=f"Ввод места хранения файлов ton_storage (по умолчанию: /var/tonstorage/)",
+                    default="/var/tonstorage",
+                ),
+                Text(
+                    name="storage_disk_space",
+                    message="Какой размер от свободного размера диска может занять ton_storage в ГБ? (по умолчанию 90% от диска)",
+                    default=get_disk_free_space() * 0.9
+                )
+            ]))
 
-        case "ton_tunnel_provider":
-            answers.append(inquirer.prompt([traffic_cost]))
+        case "TonTunnelProvider":
+            answers.append(inquirer.prompt([
+                Text(
+                    name="traffic_cost",
+                    message="Сколько будет стоить 1 Гб трафика сети?"
+                )
+            ]))
 
     return Dict(*answers)
 
 
 def main():
     answers: dict = ask()
-
-    match answers["utils_selection"]:
-        case "ton_storage":
-            return ton_storage.install(**answers)
-        case "ton_storage_provider":
-            return ton_storage_provider.install(**answers)
-        case "ton_tunnel_provider":
-            return ton_tunnel_provider.install(**answers)
+    match answers.get("util"):
+        case "TonStorage": return ton_storage.install(**answers)
+        case "TonStorageProvider": return ton_storage_provider.install(**answers)
+        case "TonTunnelProvider": return ton_tunnel_provider.install(**answers)
 
 
 if __name__ == "__main__":
