9d456212b669d3db4c3f67c3b106f02f94a71dd8
ness <shonness98@gmail.com>
2025-09-16 23:30:11 +0500

    Refactor install script to support non-interactive arguments.

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,6 +11,11 @@ ignore=false
 COLOR='\033[92m'
 ENDC='\033[0m'
 
+modules=""
+storage_path=""
+storage_cost=""
+space_to_provide=""
+
 # functions
 show_help_and_exit() {
 	echo 'Supported arguments:'
@@ -18,37 +23,37 @@ show_help_and_exit() {
 	echo ' -a               Set MyTonProvider git repo author'
 	echo ' -r               Set MyTonProvider git repo'
 	echo ' -b               Set MyTonProvider git repo branch'
+	echo ' -m               Comma-separated modules list'
+	echo ' -p               Storage path'
+	echo ' -c               Storage cost per GB'
+	echo ' -s               Space to provide in GB'
 	echo ' -i               Ignore non-root user checking'
 	echo ' -h               Show this help'
 	exit
 }
 
 restart_yourself_via_root() {
-	# Check for input_user
-	if [[ "${input_user}" != "" ]]; then
-		return
-	fi
+  if [[ -n "${input_user}" ]]; then
+    return
+  fi
 
-	# Get vars
-	user=$(whoami)
-	user_id=$(id -u)
-	user_groups=$(groups ${user})
+  user=$(whoami)
+  user_id=$(id -u)
+  user_groups=$(groups "${user}")
 
-	# Check is running as a normal user
-	if [[ ${user_id} == 0 ]] && [[ ${ignore} == false ]]; then
-		echo "Please run script as non-root user. You can create a new non-root user with command 'sudo adduser'. Or use flag '-i' to ignore this check."
-		exit 1
-	fi
+  if [[ ${user_id} == 0 ]] && [[ ${ignore} == false ]]; then
+    echo "Please run script as non-root user. You can create a new non-root user with command 'sudo adduser'. Or use flag '-i' to ignore this check."
+    exit 1
+  fi
 
-	# Using sudo or su
-	cmd="bash ${0} -u ${user}"
-	if [[ ${user_groups} == *"sudo"* ]]; then
-		sudo ${cmd}
-		exit
-	else
-		su root -c "${cmd}"
-		exit
-	fi
+  cmd=(bash "$0" -u "$user" "$@")
+  if [[ ${user_groups} == *"sudo"* ]]; then
+    sudo "${cmd[@]}"
+    exit
+  else
+    su root -c "${cmd[*]}"
+    exit
+  fi
 }
 
 # Show help for --help
@@ -57,22 +62,24 @@ if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
 fi
 
 # Input args
-while getopts "u:a:r:b:ih" flag; do
+while getopts "u:a:r:b:m:p:c:s:ih" flag; do
 	case "${flag}" in
 		u) input_user=${OPTARG};;
 		a) author=${OPTARG};;
 		r) repo=${OPTARG};;
 		b) branch=${OPTARG};;
+		m) modules=${OPTARG};;
+		p) storage_path=${OPTARG};;
+		c) storage_cost=${OPTARG};;
+		s) space_to_provide=${OPTARG};;
 		i) ignore=true;;
 		h) show_help_and_exit;;
-		*)
-			echo "Flag -${flag} is not recognized. Aborting"
-		exit 1 ;;
+		*) echo "Flag -${flag} is not recognized. Aborting"; exit 1 ;;
 	esac
 done
 
 # Reboot yourself via root to continue the installation
-restart_yourself_via_root
+restart_yourself_via_root "$@"
 
 # Continue the installation
 user=${input_user}
@@ -113,7 +120,22 @@ install_pip_dependencies() {
 }
 
 launch_installer() {
-	python3 ${src_path}/install.py --user ${user} --src_dir ${src_dir} --bin_dir ${bin_dir} --venvs_dir ${venvs_dir} --venv_path ${venv_path} --src_path ${src_path}
+	cmd="python3 ${src_path}/install.py --user ${user} --src_dir ${src_dir} --bin_dir ${bin_dir} --venvs_dir ${venvs_dir} --venv_path ${venv_path} --src_path ${src_path}"
+
+	if [[ -n "${modules}" ]]; then
+		cmd="${cmd} --utils ${modules}"
+	fi
+	if [[ -n "${storage_path}" ]]; then
+		cmd="${cmd} --storage_path ${storage_path}"
+	fi
+	if [[ -n "${storage_cost}" ]]; then
+		cmd="${cmd} --storage_cost ${storage_cost}"
+	fi
+	if [[ -n "${space_to_provide}" ]]; then
+		cmd="${cmd} --space_to_provide_gigabytes ${space_to_provide}"
+	fi
+
+	eval ${cmd}
 }
 
 mytonprovider_setup() {
@@ -130,7 +152,7 @@ mytonprovider_setup() {
 	echo -e "${COLOR}[4/6]${ENDC} Installing requirements"
 	install_pip_dependencies
 
-	echo -e "${COLOR}[5/6]${ENDC} Launching UI/UX installer"
+	echo -e "${COLOR}[5/6]${ENDC} Launching installer"
 	launch_installer
 
 	echo -e "${COLOR}[6/6]${ENDC} MyTonProvider installation completed"
