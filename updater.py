c67b60b0aa5ca4392003a1cc76484dcbf54ea7d5
ness <shonness98@gmail.com>
2025-12-05 03:51:35 +0500

    update

--- updater.py
+++ updater.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
 	#local.run()
 	import_modules(local)
 	auto_updater_module = get_module_by_name(local, "auto-updater")
-	local.cycle(func=auto_updater_module.update_modules, sec=60 * 10, args=None)
+	local.start_cycle(func=auto_updater_module.update_modules, sec=60 * 10, args=None)
 #end if
