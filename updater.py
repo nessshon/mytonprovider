244330337e6c5b28b07b6616b8aefc2186b6f2ce
ness <shonness98@gmail.com>
2025-12-06 19:58:19 +0500

    update

--- updater.py
+++ updater.py
@@ -1,31 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf_8 -*-
 
-import time
-import traceback
-
 from mypylib import MyPyClass
 from utils import (
 	import_modules,
 	get_module_by_name
 )
 
-INTERVAL = 10 * 60
-
 if __name__ == "__main__":
 	local = MyPyClass(__file__)
 	#local.run()
 	import_modules(local)
 	auto_updater_module = get_module_by_name(local, "auto-updater")
-
-	while True:
-		try:
-			auto_updater_module.update_modules()
-		except Exception as e:
-			local.add_log(f"[auto-updater] error: {e!r}")
-			tb = traceback.format_exc()
-			local.add_log(f"[auto-updater] traceback:\n{tb}")
-
-		local.add_log(f"[auto-updater] sleep {INTERVAL} seconds")
-		time.sleep(INTERVAL)
+	local.cycle(func=auto_updater_module.update_modules, sec=5 * 60, args=None)
 #end if
